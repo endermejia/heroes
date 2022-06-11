ea8723d170002088d93538900785a34953f172a8
Gabriel Mejia <endermejia032@gmail.com>
2022-06-11 19:52:04 +0200

    FEATURE: show galardon info

--- src/app/nobel/pages/galardon/galardon.component.ts
+++ src/app/nobel/pages/galardon/galardon.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NobelService} from '../../services/nobel.service';
-import {GalardonLabelsModel} from '../../models/nobel.model';
+import {GalardonLabelsModel, LaureateModel} from '../../models/nobel.model';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
@@ -37,16 +37,37 @@ export class GalardonComponent implements OnInit {
     this.getNobelPrize();
   }
 
-  getAwardYear(): string {
+  public getLaureateNames(laureate: LaureateModel): string {
+    return laureate.knownName?.en || laureate.orgName?.en || '-';
+  }
+
+  public getLaureateContribution(laureate: LaureateModel): string {
+    return laureate.portion
+      ? (Number(this.getPortionNumberByStringFraction(laureate.portion)) * 100) + ' %'
+      : '-';
+  }
+
+  public getLaureateMotive(laureate: LaureateModel): string {
+    return laureate.motivation?.en || '-';
+  }
+
+  private getPortionNumberByStringFraction(portion: string): Number {
+    const fraction = portion.split('/');
+    const fraction0 = Number(fraction[0]) || 0;
+    const fraction1 = Number(fraction[1]) || 0;
+    return fraction.length > 1 ? fraction0 / fraction1 : fraction0;
+  }
+
+  private getAwardYear(): string {
     return this.activatedRoute.snapshot.paramMap.get('awardYear')?.split('-').pop() || '';
   }
 
-  getCategory(): string {
+  private getCategory(): string {
     const category = this.activatedRoute.snapshot.paramMap.get('awardYear')?.split('-').shift() || '';
     return this.categories[category];
   }
 
-  getNobelPrize(): void {
+  private getNobelPrize(): void {
     this.nobelService.showSpinner = true;
     this.nobelService.searchNobelPrizeByCategoryAndYear(this.getCategory(), this.getAwardYear())
   }
